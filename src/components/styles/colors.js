3b9c89c26e84241a2515a2b52000c66d6a280fe5
Dunia Goncalves <duniaimara@gmail.com>
2025-02-15 15:58:19 -0500

    set light/dark theme move to own files

---
+++ src/components/styles/colors.js
@@ -0,0 +1,11 @@
+export const softOffWhite = '#F7F6F3'; 
+export const warmCharGray = '#3D3A36'; 
+export const pastelTeal = '#A3C4BC';
+export const softPeach = '#E7C8B3'
+export const dustyPink = '#DAB1C5'
+export const lightGray = '#D9D9D9';
+
+export const deepGray = '#2C2A32';
+export const warmOffWhite = '#EAE8E1';
+export const beige = '#E7C8B3';
+export const softGray = '#5A585F';
---
+++ src/components/styles/themes.jsx
@@ -0,0 +1,16 @@
+import { softOffWhite, warmCharGray, pastelTeal, softPeach, dustyPink, lightGray, deepGray, warmOffWhite, beige, softGray } from "./colors";
+
+export const lightTheme = {
+  primaryBg: `${softOffWhite}`,
+  secondaryBg: `${warmOffWhite}`,
+  textColor: `${warmCharGray}`,
+  accentColor: `${pastelTeal}`,
+  buttonBg: `${dustyPink}`,
+  buttonText: `${deepGray}`,
+};
+
+export const darkTheme = {
+  primaryBg: `${deepGray}`,
+  secondaryBg: `${warmCharGray}`,
+  textColor: `${warmOffWhite}`,
+};
